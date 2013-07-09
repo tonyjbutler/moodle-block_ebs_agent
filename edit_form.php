dc048c9e5f40d00d68bf0c1b6a88f52ff3ffd177
Tony Butler <tonyjbutler@gmail.com>
2013-07-09 16:53:46 +0100

    Add calls to setType for config form fields

--- edit_form.php
+++ edit_form.php
@@ -8,16 +8,21 @@ class block_ebs_agent_edit_form extends block_edit_form {
 		$mform->addElement("header", "configheader", "Settings");
  
 		$mform->addElement("text", "config_db_host", "TNS name");
+        $mform->setType('config_db_host', PARAM_TEXT);
 		$mform->addRule("config_db_host", "TNS name is required.", "required", "", "server", false, false);
 
 		$mform->addElement("text", "config_db_user", "Username");
+        $mform->setType('config_db_user', PARAM_TEXT);
 		$mform->addRule("config_db_user", "Username is required.", "required", "", "server", false, false);
 
 		$mform->addElement("password", "config_db_password", "Password");
 		$mform->addRule("config_db_password", "Password is required", "required", "server", false, false);
 		
 		$mform->addElement("text", "config_header_image_url", "Header image URL");
+        $mform->setType('config_header_image_url', PARAM_TEXT);
+
 		$mform->addElement("text", "config_header_text", "Header text");
+        $mform->setType('config_header_text', PARAM_TEXT);
 
 	}
 }
